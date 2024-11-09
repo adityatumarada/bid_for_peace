36127ab57d0e322697fbfd1a003ca0077fddd278
Aditya Tumarada <adityatumarada@gmail.com>
2024-11-09 17:21:34 +0530

    Added logout button.

--- bid_for_peace/urls.py
+++ bid_for_peace/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from auction.views import admin_auction_view, user_auction_view, login_view, logout_view, start_auction, end_auction, place_bid
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view, name='login_view'),
-    path('logout/', logout_view, name='logout_view'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin_auction/', admin_auction_view, name='admin_auction_view'),
     path('auction/', user_auction_view, name='user_auction_view'),
     path('start_auction/<int:player_id>/', start_auction, name='start_auction'),
